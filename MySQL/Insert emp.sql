a12245288bab88b0e8e082725e8b272ced0b183e
Min <70638717+sou05091@users.noreply.github.com>
2023-04-18 12:51:40 +0900

    Add files via upload

---
+++ MySQL/Insert emp.sql
@@ -0,0 +1,6 @@
+insert into world.emp (eno, ename, dno, salary)
+values
+('E1', 'Lopez', 'D1', '4000000'),
+('E2', 'Cheng', 'D1', '4200000'),
+('E3', 'Finzi', 'D2', '3000000'),
+('E4', 'Saito', 'D2', '3500000');
---
+++ MySQL/Selet from s.sql
@@ -0,0 +1,4 @@
+select * from s;
+select sname city from s;
+select sname from s where city = 'paris';
+select sname from s where sno='s1';

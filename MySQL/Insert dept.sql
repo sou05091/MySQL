1802cc28a9c2c59b3da4125cb85a063b064839e1
Min <70638717+sou05091@users.noreply.github.com>
2023-04-25 10:18:54 +0900

    Delete MySQL directory

--- MySQL/Insert dept.sql
+++
@@ -1,5 +0,0 @@
-insert into world.dept (dno, dname, budget)
-values
-('D1', 'Marketing', 100000),
-('D2', 'Development', 120000),
-('D3', 'Research', 50000);
--- MySQL/Selet from s.sql
+++
@@ -1,4 +0,0 @@
-select * from s;
-select sname city from s;
-select sname from s where city = 'paris';
-select sname from s where sno='s1';
